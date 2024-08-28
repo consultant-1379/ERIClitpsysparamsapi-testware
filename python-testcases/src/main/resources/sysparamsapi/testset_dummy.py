6b74a269ebe03cc9cae8fa8bac22d1190fb4c8c3
consultant <consultant-1379@tutamail.com>
2024-08-28 11:51:18 +0800

    first commit

---
+++ python-testcases/src/main/resources/sysparamsapi/testset_dummy.py
@@ -0,0 +1,92 @@
+'''
+COPYRIGHT Ericsson 2019
+The copyright to the computer program(s) herein is the property of
+Ericsson Inc. The programs may be used and/or copied only with written
+permission from Ericsson Inc. or in accordance with the terms and
+conditions stipulated in the agreement/contract under which the
+program(s) have been supplied.
+
+@summary:   Integration test for XXX
+'''
+
+from litp_generic_test import GenericTest, attr
+import test_constants
+
+
+class StoryXXX(GenericTest):
+    """
+    DELETE THIS TEST
+    """
+
+    def setUp(self):
+        """Run before every test"""
+        super(StoryXXX, self).setUp()
+
+    def tearDown(self):
+        """Run after every test"""
+        super(StoryXXX, self).tearDown()
+
+    @attr('all', 'cdb_tmp')
+    def test_01_p_xxx(self):
+        """
+            Descritpion:
+                fdhsdjh
+            Actions:
+                1.
+            Result:
+        """
+
+        print "DELETE ME AND REPLACE WITH A REAL LITP TEST"
+        self.assertEqual([], [])
+
+    @attr('cdb_tmp')
+    def test_02_p_xxx(self):
+        """
+            Descritpion:
+                fdhsdjh
+            Actions:
+                1.
+            Result:
+        """
+
+        print "DELETE ME AND REPLACE WITH A REAL LITP TEST"
+        self.assertEqual([], ['ksdjf'])
+
+    @attr('cdb_tmp')
+    def test_03_p_xxx(self):
+        """
+            Descritpion:
+                fdhsdjh
+            Actions:
+                1.
+            Result:
+        """
+
+        print "DELETE ME AND REPLACE WITH A REAL LITP TEST"
+        self.assertEqual([], [])
+
+    @attr('pre-reg', 'cdb_tmp')
+    def test_04_p_xxx(self):
+        """
+            Descritpion:
+                fdhsdjh
+            Actions:
+                1.
+            Result:
+        """
+
+        print "DELETE ME AND REPLACE WITH A REAL LITP TEST"
+        self.assertEqual([], ["fdjklhfsd"])
+
+    @attr('all', 'cdb_tmp')
+    def test_05_p_xxx(self):
+        """
+            Descritpion:
+                fdhsdjh
+            Actions:
+                1.
+            Result:
+        """
+
+        print "DELETE ME AND REPLACE WITH A REAL LITP TEST"
+        self.assertEqual([], [])
